2c3704e7456966bc0100cb99be35da6da7a8457b
yuta <az7xj5xn@yahoo.co.jp>
2020-11-25 20:05:25 +0900

    seeds.rbに初期データを追加しました。

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+for n in 1..10 do
+  Post.create(content: "No#{n} sample tweet")
+end
