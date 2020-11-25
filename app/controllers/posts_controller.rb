ab3c203adbb179a2ed884a33decb89241162c567
yuta <az7xj5xn@yahoo.co.jp>
2020-11-25 09:34:24 +0900

    createアクションを追加しました。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,15 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    @post = Post.new(params.require(:post).permit(:content))
+    if @post.save
+      redirect_to posts_path
+    else
+      render 'new'
+    end
+  end
+  
   def edit
     @post = Post.find(params[:id])
   end
